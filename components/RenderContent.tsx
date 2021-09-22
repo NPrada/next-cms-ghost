91c0d303d3705718eb94fb8f8b21bf472adb6d12
Niccolò <niccolo.prada@2iqresearch.com>
2021-09-22 11:38:58 +0100

    add expandable images

--- components/RenderContent.tsx
+++ components/RenderContent.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import rehypeReact, { ComponentProps, ComponentPropsWithNode } from 'rehype-react'
 import unified from 'unified'
 import { Node } from 'unist'
 
 import { NextLink } from '@components/NextLink'
 import { NextImage } from '@components/NextImage'
+
+const WrappedImage = (props: any) => {
+
+  const [isImageOpened, setImageOpened] = useState(false)
+
+  return (
+    <div className={isImageOpened ? "post-full-image__expanded" : ''} onClick={() => {setImageOpened(!isImageOpened)}}>
+    <NextImage {...props}/>
+  </div>
+  )
+}
 
 /* eslint-disable react/display-name */
 const options = {
@@ -13,7 +24,7 @@ const options = {
   passNode: true,
   components: {
     Link: (props: ComponentProps) => <NextLink {...(props as ComponentPropsWithNode)} />,
-    Image: (props: ComponentProps) => <NextImage {...(props as ComponentPropsWithNode)} />,
+    Image: (props: ComponentProps) => <WrappedImage {...(props as ComponentPropsWithNode)} />,
   },
 }
 
